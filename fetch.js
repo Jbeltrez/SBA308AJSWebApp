23c6a3c587b800496040eb5a4314f3c55a0008e0
john beltrez <beltrezjohn1@gmail.com>
2024-10-30 11:46:40 -0500

    Created main.js index.html styles.css

---
+++ fetch.js
@@ -0,0 +1,8 @@
+
+const API_KEY = "live_PwQyLmNZR9H9elcnOZ7A8Dr9ib54diZy9wt75CtLd6ntVJmt6swh2ww5qseKB7QU";
+
+const URL = `https://api.thedogapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=${API_KEY}`
+
+
+
+
